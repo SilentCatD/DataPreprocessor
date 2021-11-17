53ce93f2bb0f3f6b5c2c272be9aa567e7b4ab4b5
Silent Cat <ngocphuocnguyen312@gmail.com>
2021-11-17 08:53:05 +0700

    restructure

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,4 +4,19 @@ import argparse
 
 if __name__ == '__main__':
     """Entry point to interact with the commandline arguments"""
-    pass
+    main_parser = argparse.ArgumentParser(
+        description="Simple program to do csv file pre-processing",
+        epilog="Thank you for using",
+        allow_abbrev=False,
+    )
+    main_parser.add_argument(
+        "data_file",
+        help="Name of the data file to be pre-processed",
+        action='store',
+    )
+    main_parser.add_argument(
+        '-v',
+        '--version',
+        action='version'
+    )
+    main_parser.parse_args()
