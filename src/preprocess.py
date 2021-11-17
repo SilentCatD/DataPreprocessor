fec601bd67ec784c1a2a491e8b11ff67704df99a
Silent Cat <ngocphuocnguyen312@gmail.com>
2021-11-17 20:52:27 +0700

    finished delete duplicate CLI

--- src/preprocess.py
+++ src/preprocess.py
@@ -49,40 +49,66 @@ def fill_na_func(fill_args):
     print("done!")
 
 
+def delete_duplicate(deldup_args):
+    print(vars(deldup_args))
+    processor = DataPreprocessor(deldup_args.file)
+    if deldup_args.outfile:
+        if not deldup_args.outfile.endswith('.csv'):
+            raise NameError("output filename must end with '.csv'")
+    if deldup_args.type == 'row':
+        print("deleting duplicate row...")
+        processor.delete_duplicate_row(deldup_args.outfile)
+    if deldup_args.outfile:
+        print(f"Saved to {deldup_args.outfile}")
+    else:
+        print(f"Saved to {deldup_args.file}")
+    print("done!")
+
+
 if __name__ == '__main__':
     """Entry point to interact with the commandline arguments"""
 
     # Main parser, do general stuff
     main_parser = argparse.ArgumentParser(description="Simple program to do csv file pre-processing",
                                           epilog="Thank you for using", allow_abbrev=False, )
-    main_parser.add_argument('-f', '--file', help="Name of the data file to be pre-processed", action='store',
+    main_parser.add_argument('-f', '--file', help="name of the data file to be pre-processed", action='store',
                              required=True, metavar='')
     main_parser.add_argument('-v', '--version', action='version', version='preprocessor version 1.0.0', )
     main_parser.set_defaults(func=undefined)
-    sub_parser = main_parser.add_subparsers(help="use <option> -h to view more info", )
+
+    # add subparsers
+    sub_parsers = main_parser.add_subparsers(help="use %(prog)s <option> -h to see usage of each option", )
     # show info about rows and cols: 1, 2
-    list_parser = sub_parser.add_parser("list")
+    list_parser = sub_parsers.add_parser("list",
+                                         help="list the information about data such as missing cols, missing rows,...")
     list_parser.add_argument('-mr', '--missing-rows', help="list missing rows", action='store_true')
     list_parser.add_argument('-mc', '--missing-cols', help="list missing columns", action='store_true')
     list_parser.add_argument('-m', '--missing', help="list missing info", action='store_true')
     list_parser.set_defaults(func=list_func)
 
     # fill nan value: 3
-    fill_parser = sub_parser.add_parser("fill")
+    fill_parser = sub_parsers.add_parser("fill", help="fill the missing N/A value of the data with specified type")
     fill_parser.add_argument("-ft", '--filltype',
-                             help="Set the fill type for NUMERIC value, must be one of [mean, median]", required=True,
+                             help="set the fill type for NUMERIC value, must be one of [mean, median]", required=True,
                              choices=["mean", "median"], metavar='')
-    fill_parser.add_argument("-fb", "--fallback", help="Set fallback value if fill failed, default value will be 0",
+    fill_parser.add_argument("-fb", "--fallback", help="Set fallback value if fill failed, default value will be '0'",
                              metavar='', default='0')
     fill_parser.add_argument("-o", "--outfile",
-                             help="Set the name of the output file, if not specified, the current file will be "
+                             help="set the name of the output file, if not specified, the current file will be "
                                   "overwritten", metavar='')
     fill_parser.set_defaults(func=fill_na_func)
 
     # delete with threshold: 4, 5
 
     # delete duplicate: 6
-
+    delete_duplicate_parser = sub_parsers.add_parser("deldup", help="delete duplicate data")
+    delete_duplicate_parser.add_argument('-t', '--type', choices=['row'],
+                                         help='Choose the type of duplicate deletion, must be one of ["rows"]',
+                                         metavar='', required=True)
+    delete_duplicate_parser.add_argument("-o", "--outfile",
+                                         help="set the name of the output file, if not specified, the current file "
+                                              "will be overwritten", metavar='')
+    delete_duplicate_parser.set_defaults(func=delete_duplicate)
     # standardization: 7
 
     # attribute calc
